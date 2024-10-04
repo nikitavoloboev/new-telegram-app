9dfc97c71a7b4441fb4f607aa320718876c0516d
Nikita <github@nikiv.dev>
2024-10-04 11:35:45 +0300

    local dev

--- app.config.ts
+++ app.config.ts
@@ -1,17 +1,15 @@
 import { defineConfig } from "@tanstack/start/config"
-import basicSsl from "@vitejs/plugin-basic-ssl"
-import react from "@vitejs/plugin-react"
 import { readFileSync } from "node:fs"
 import { resolve } from "node:path"
-import { nodePolyfills } from "vite-plugin-node-polyfills"
 import tsConfigPaths from "vite-tsconfig-paths"
 
-const local = process.env.LOCAL === "true"
+// const local = process.env.LOCAL === "true"
+const local = true
 
-// TODO: issues with nodePolyfills, have to make all components client side only for this
 export default defineConfig({
   vite: {
     plugins: () => [
+      // TODO: issues with nodePolyfills, have to make all components client side only for this
       // nodePolyfills({
       //   include: ["path", "stream", "util"],
       //   exclude: ["http"],
@@ -25,24 +23,16 @@ export default defineConfig({
       //   },
       //   protocolImports: true,
       // }),
-      react(),
       tsConfigPaths({
         projects: ["./tsconfig.json"],
       }),
-      ...(local !== true
-        ? [
-            basicSsl({
-              certDir: resolve("certificates"),
-              domains: ["tma.internal"],
-            }),
-          ]
-        : []),
     ],
   },
   server: {
-    // host: "tma.internal",
-    https:
-      local === true
+    devServer: {
+      // @ts-ignore
+      host: "tma.internal",
+      https: local
         ? {
             cert: readFileSync(
               resolve("certificates/tma.internal.pem"),
@@ -54,5 +44,6 @@ export default defineConfig({
             ),
           }
         : undefined,
+    },
   },
 })
