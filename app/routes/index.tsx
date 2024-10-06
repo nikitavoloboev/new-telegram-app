1b62c3e3753af7d5fa17d2466d2080038593dc19
Nikita <github@nikiv.dev>
2024-10-06 14:22:52 +0300

    .

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,9 +1,9 @@
 import { createFileRoute } from "@tanstack/react-router"
 
-export const Route = createFileRoute("/")({
-  component: Home,
-})
-
-function Home() {
+function RouteComponent() {
   return <div></div>
 }
+
+export const Route = createFileRoute("/")({
+  component: RouteComponent,
+})
